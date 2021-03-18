8129dfedc92ceab3e32157874ee79458a5525f17
quantung22124102 <79367005+quantung22124102@users.noreply.github.com>
2021-03-18 18:24:57 +0700

    Add files via upload

---
+++ A2a.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main()
+{
+	string str;
+	cin>>str;
+	cout<<str<<endl;
+	for(int i=0;i<=str.length();i++);
+	cout<<str;
+	return 0;
+}
---
+++ A2b.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main()
+{
+	string str="abc";
+	cout<<str<<endl;
+	for(int i=0;i<str.length();i++);
+	cout<<str;
+	return 0;
+}
